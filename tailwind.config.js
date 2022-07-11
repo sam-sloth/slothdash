f3493c00cb498a38c0bd57e89bf289158dd7371d
sam-sloth <user@DESKTOP-DPJ8BLP>
2022-07-11 15:57:32 -0500

    add base color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: {
+          950: '#130329',
+        },
+        purple: {
+          950: '#5A3E79',
+        },
+        mauve: {
+          950: '#A76286',
+        },
+        coral: {
+          950: '#FCA7A5',
+        },
+      },
+    },
   },
   plugins: [],
 };
